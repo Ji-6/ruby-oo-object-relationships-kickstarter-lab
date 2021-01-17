9940d6233abde8898fe2451f2e90f84c43bc0a22
Jishan Ekram <ekram.jishan@gmail.com>
2021-01-17 18:03:18 -0500

    commit tree 5eb956d58455372bc8e8ca03ff037a238cccaa47

---
+++ lib/project_backer.rb
@@ -0,0 +1,15 @@
+class ProjectBacker
+    attr_reader :backer, :project
+
+    @@all = []
+
+    def initialize(backer, project)
+        @backer = backer
+        @project = project
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+end 
